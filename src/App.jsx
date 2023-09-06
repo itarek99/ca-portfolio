c6f0a3ade56cd869882f36da0768b16a3266e71a
itarek99 <tarekulislam7060@gmail.com>
2023-09-06 12:08:29 +0600

    bg gradient added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Gallery from './components/Gallery';
 import Heading from './components/Heading';
 import ServiceDropdown from './components/ServiceDropdown';
 import ServiceList from './components/ServiceList';
+import Testimonials from './components/Testimonials';
 import services from './data/portfolio';
 import { setSelectedService } from './features/portfolioSlice';
 
@@ -20,7 +21,7 @@ function App() {
   return (
     <>
       <Heading />
-      <main className='font-roboto relative bg-[#F7F9FF] xl:bg-gradient-to-r from-[#FCFCFC] from-[36%] 2xl:from-[40%]  via-[#F7F9FF] via-[36%] 2xl:via-[40%] to-[#F7F9FF] to-100% pt-12'>
+      <section className='font-roboto relative bg-[#F7F9FF] xl:bg-gradient-to-r from-[#FCFCFC] from-[36%] 2xl:from-[40%]  via-[#F7F9FF] via-[36%] 2xl:via-[40%] to-[#F7F9FF] to-100% pt-12'>
         <div className='container mx-auto px-4 xl:px-0 grid grid-cols-12'>
           <div className=' lg:hidden col-span-12 mb-8'>
             <div className='font-semibold text-2xl text-[#2C2C2C] mb-3'>Services</div>
@@ -29,12 +30,9 @@ function App() {
           <div className='hidden lg:block col-span-4'>
             <ServiceList imageBox={imageBox} />
           </div>
-          <div className='col-span-12 lg:col-span-8 lg:ml-20'>
+          <div className='col-span-12 lg:col-span-8 lg:ml-20 mb-24 lg:mb-0'>
             <Gallery imageBox={imageBox} />
           </div>
-        </div>
-        <div>
-          <CustomQuote />
         </div>
         <div className='hidden xl:block absolute top-0 right-0'>
           <img src={dotMatrix} alt='dot matrix' />
@@ -42,7 +40,13 @@ function App() {
         <div className='absolute top-0 right-0'>
           <img src={ellipse} alt='bg ellipse' />
         </div>
-      </main>
+        <div
+          id='gallery-shape'
+          className='absolute left-0 lg:left-[36%] 2xl:left-[40%] bg-gradient-to-r from-[#FEE6F9] to-transparent top-3/4 lg:top-0 bottom-0 lg:bottom-52 right-[20%]'
+        ></div>
+      </section>
+      <Testimonials />
+      <CustomQuote />
     </>
   );
 }
---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,15 @@
+const Testimonials = () => {
+  return (
+    <div className='container mx-auto mt-20'>
+      <div className='flex flex-col items-center gap-2 justify-center'>
+        <h2 className='text-2xl xl:text-4xl font-semibold relative font-roboto'>
+          What Out <span className='text-orange-500'>Client</span> Says
+        </h2>
+        <p className='text-gray-500 text-base font-roboto'>
+          Hear From Those Who Have Benefited from Our Professional Photo Editing Services.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Testimonials;
